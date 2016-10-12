1d71e64a743ad9580adf52b0bd9f99d322c6cb10
Dominic Ortega <dortega37@cnm.edu>
2016-10-12 12:39:01 -0600

    Added an img

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Data-Design-Okayplayer</title>
+	</head>
+	<body>
+
+		<!-- this is the header -->
+		<header>
+			<h1>Persona</h1>
+		</header>
+
+		<!-- this is the description of Persona -->
+		<main>
+
+			<!-- begin Persona -->
+
+			<!-- begin description -->
+			<section>
+				<p>When I look at Okayplayer.com I imagine a multigenerationalist who has a hunger for various musical and entertainment types. I imagine someone who likes to feed their senses with news feeds, audio and videos. I imagine someone who has an opinion and is reactive to what is digested. This person is a professional who uses a variety of mediums of technology. This busy professional keeps their cool in all stressful situations and likes to use the site to keep up with the latest our site has to offer.</p>
+			</section>
+
+			<!-- begin needs, goals and features -->
+			<section>
+				<h2>Needs, Goals And Features</h2>
+				<ul>
+					<li>Have highlighted features standout</li>
+					<li>Lots of information in a short period of time</li>
+					<li>Stay up to date</li>
+					<li>Create links to all related stories</li>
+					<li>"I don't have a lot of time."</li>
+					<li>"Use more pics."</li>
+					<li>Persona may like more stories about their favorite artists.</li>
+				</ul>
+			</section>
+		</main>
+	</body>
+
+
+</html>
