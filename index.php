9f8e6974f7de8b94291a4601b02df8d8393c3de9
Dominic Ortega <dortega37@cnm.edu>
2016-10-12 13:06:46 -0600

    Changed <h3> to <h2> for Use Case.

--- index.php
+++ index.php
@@ -6,18 +6,21 @@
 	</head>
 	<body>
 
+		<!-- begin persona -->
+
 		<!-- this is the header -->
 		<header>
 			<h1>Persona</h1>
 		</header>
 
-		<!-- this is the description of Persona -->
+		<!-- this is the description of persona -->
 		<main>
 
-			<!-- begin Persona -->
+
 
 			<!-- begin description -->
 			<section>
+				<img src="img/images.jpeg" alt="images">
 				<p>When I look at Okayplayer.com I imagine a multigenerationalist who has a hunger for various musical and entertainment types. I imagine someone who likes to feed their senses with news feeds, audio and videos. I imagine someone who has an opinion and is reactive to what is digested. This person is a professional who uses a variety of mediums of technology. This busy professional keeps their cool in all stressful situations and likes to use the site to keep up with the latest our site has to offer.</p>
 			</section>
 
@@ -33,6 +36,18 @@
 					<li>"Use more pics."</li>
 					<li>Persona may like more stories about their favorite artists.</li>
 				</ul>
+			</section>
+
+			<!-- begin use case -->
+
+			<!-- this is the header -->
+		<header>
+			<h2>Use Case</h2>
+		</header>
+
+			<!-- this is the description of use case -->
+			<section>
+				<p>Our use case is a busy person who, when is not on the go, is busy at work. Our u.c. can utilize our site any time of day, anywhere in the world, on any device they have access to. Our Persona is going to utilize our site to keep up with their favorit artists and remain conscious to the world outside of entertainment all at the same time. The Persona is going to use our site to find out what their purchase spectrum looks like.</p>
 			</section>
 		</main>
 	</body>
