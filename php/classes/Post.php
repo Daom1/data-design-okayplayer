e9138048dfe015b935806b19cfb771ea902df4ec
Dominic Ortega <dortega37@cnm.edu>
2016-10-19 13:20:59 -0600

    I think i'm done

---
+++ php/classes/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *
+ */
+@author Dominic Ortega <dortega37@cnm.edu>
+@version 1.0.0
+
+
+class Post {
+	private $postId;
+	private $postContent;
+	private $postDateTime;
+	private $postTitle;
+	private $postAuthor;
